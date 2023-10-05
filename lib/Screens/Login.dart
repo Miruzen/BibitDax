c1081671237515c0ac8d7809404fa3496e594273
Miruzen <nandagouwputra@gmail.com>
2023-10-05 14:37:16 +0700

    Username , Pass

--- lib/Screens/Login.dart
+++ lib/Screens/Login.dart
@@ -8,6 +8,9 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+
+  TextEditingController UsernameController  = TextEditingController();
+  TextEditingController passwordController = TextEditingController() ; 
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,8 +19,8 @@ class _LoginState extends State<Login> {
               begin: Alignment.topRight,
               end: Alignment.bottomLeft,
               colors: [
-            Color.fromRGBO(0, 255, 102, 0.863),
-            Color.fromRGBO(30, 146, 65, 0.816),
+            Color.fromRGBO(176, 255, 144, 0.863),
+            Color.fromRGBO(21, 179, 113, 0.816),
           ])),
       child: Scaffold(
         backgroundColor: Colors.transparent,
@@ -27,20 +30,67 @@ class _LoginState extends State<Login> {
   }
 
   Widget _page() {
-    return Center(
-      child: Column(children: [
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child : Center(
+      child: Column(
+        children: [
         _icon(),
-      ]
-      ),
+        const SizedBox(height: 50) ,
+        _inputField1("Enter E-mail",  UsernameController) , 
+        const SizedBox(height: 30) ,
+        _inputField2("Enter Password", passwordController , isPassword : true    ) , 
+        const SizedBox(height : 8 ) , 
+          ]  
+        ),
+      )
     );
   }
 
   Widget _icon() {
     return Container(
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.white, width: 2),
-          shape: BoxShape.circle),
-      child: const Icon(Icons.wallet_outlined, color: Colors.white, size: 120),
+      margin: EdgeInsets.all(16.0),
+      child:  Image.asset('images/Wallet.png', height: 210 , scale:  2.5, ) , 
+      
     );
+  }
+
+  Widget _inputField1(String Text , TextEditingController controller ,){
+
+    var border = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(80) , 
+      borderSide: const BorderSide(color: Colors.white),
+    );
+    return TextField(
+      style: const TextStyle( color: Colors.white),
+      controller: controller,
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.email) ,prefixIconColor: Color.fromRGBO(50, 50, 50, 40), 
+        hintText: Text , 
+        hintStyle: const TextStyle(color: Colors.white ),
+        enabledBorder: border, 
+        focusedBorder: border ,
+      ),
+    ) ; 
+  }
+  Widget _inputField2(String Text , TextEditingController controller ,
+  {isPassword = false }){
+
+    var border = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(80) , 
+      borderSide: const BorderSide(color: Colors.white),
+    );
+    return TextField(
+      style: const TextStyle( color: Colors.white),
+      controller: controller,
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.lock),prefixIconColor: Color.fromRGBO(50, 50, 50, 40), 
+        hintText: Text , 
+        hintStyle: const TextStyle(color: Colors.white ),
+        enabledBorder: border, 
+        focusedBorder: border ,
+      ),
+      obscureText: isPassword,
+    ) ; 
   }
 }
