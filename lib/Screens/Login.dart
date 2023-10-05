0e254c62af3d86e41b09c3e303230cdc72abefee
Miruzen <nandagouwputra@gmail.com>
2023-10-05 16:04:59 +0700

    Login page DOne

--- lib/Screens/Login.dart
+++ lib/Screens/Login.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -20,9 +21,10 @@ class _LoginState extends State<Login> {
               end: Alignment.bottomLeft,
               colors: [
             Color.fromRGBO(176, 255, 144, 0.863),
-            Color.fromRGBO(21, 179, 113, 0.816),
+            Color.fromRGBO(17, 176, 163, 0.816),
           ])),
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.transparent,
         body: _page(),
       ),
@@ -40,7 +42,12 @@ class _LoginState extends State<Login> {
         _inputField1("Enter E-mail",  UsernameController) , 
         const SizedBox(height: 30) ,
         _inputField2("Enter Password", passwordController , isPassword : true    ) , 
-        const SizedBox(height : 8 ) , 
+        const SizedBox(height : 50 ) , 
+        _LoginBtn() , 
+        const SizedBox(height: 20,) ,
+        _Noacc(),
+        const SizedBox(height: 10,) ,
+        Text("Dont have an account ?")
           ]  
         ),
       )
@@ -93,4 +100,51 @@ class _LoginState extends State<Login> {
       obscureText: isPassword,
     ) ; 
   }
+  Widget _LoginBtn() {
+    return ElevatedButton(
+      onPressed: () {
+        debugPrint("Username : " + UsernameController.text) ; 
+        debugPrint("Password :  " + passwordController.text)  ; 
+      }
+      , child: const SizedBox(
+        width: double.infinity,
+        child: Text(
+          "LOG IN" , 
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20),
+        )
+      ),
+      style: ElevatedButton.styleFrom(
+        shape: const StadiumBorder() , 
+        primary: Color.fromARGB(255, 228, 226, 226) ,
+        onPrimary: Colors.black , 
+        padding:  const EdgeInsets.symmetric(vertical: 16)  , 
+      ),
+    ) ; 
+  }
+
+  Widget _Noacc() {
+    return ElevatedButton(
+      onPressed: () {
+        debugPrint("Username : " + UsernameController.text) ; 
+        debugPrint("Password :  " + passwordController.text)  ; 
+      }
+      , child: const SizedBox(
+        width: 250,
+        child: Text(
+          "SIGN UP" , 
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 15),
+        )
+      ),
+      style: ElevatedButton.styleFrom(
+        shape: const StadiumBorder() , 
+        primary: Color.fromARGB(200, 228, 226, 226) ,
+        onPrimary: Colors.black , 
+        padding:  const EdgeInsets.symmetric(vertical: 10)  , 
+      ),
+    ) ; 
+  }
+
+
 }
