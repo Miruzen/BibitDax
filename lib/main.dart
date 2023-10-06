08bf240ed2eb46543ed2611cffc83094feaa529f
Miruzen <nandagouwputra@gmail.com>
2023-10-06 08:28:06 +0700

    ReadMe

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:personal_finance/Screens/Login.dart';
-import 'package:personal_finance/Screens/SignUp.dart';
-import 'package:personal_finance/Widgets/bottomnavbar.dart';
 import 'package:personal_finance/data/add_data.dart';
 
 void main() async {
